fdd65c6176c4d88a7cd11ad84e01a64cda59e022
Muzammil <mmaqsood@umd.edu>
2021-12-03 08:55:55 -0500

    Implemented Lab11 Phase 2

--- WebGoat/Content/LogInjection.aspx.cs
+++ WebGoat/Content/LogInjection.aspx.cs
@@ -8,12 +8,13 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 //using log4net;
 using System.Text;
+using log4net;
 
 namespace OWASP.WebGoat.NET
 {
     public partial class LogInjection : System.Web.UI.Page
     {
-        //ILog log = LogManager.GetLogger("NOTIFY");
+        ILog log = LogManager.GetLogger("NOTIFY");
         protected void Page_Load(object sender, EventArgs e)
         {
             lblResultMessage.Text = "";
@@ -22,7 +23,7 @@ namespace OWASP.WebGoat.NET
         public void btnSubmit_Click(object sender, EventArgs args)
         {
             string result = "Ticket Successfully Submitted!";
-            byte[] msgBytes = Encoding.Unicode.GetBytes(txtBoxMsg.Text);
+            //byte[] msgBytes = Encoding.Unicode.GetBytes(txtBoxMsg.Text);
             //log.DebugFormat("{0}", Encoding.Unicode.GetString(msgBytes));
             lblResultMessage.Text = result;
         }
